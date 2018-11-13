64cb20bf64d15efdb6bc3e84769329a3b33786d9
HolkerDev <nick.penkovskiy@gmail.com>
2018-11-13 21:56:09 +0100

    Add list activity

---
+++ app/src/main/java/com/dev/holker/wholesale/List.kt
@@ -0,0 +1,21 @@
+package com.dev.holker.wholesale
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_list.*
+
+class List : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+
+        fab.setOnClickListener {
+
+        }
+    }
+
+}
--- app/src/main/java/com/dev/holker/wholesale/Login.kt
+++ app/src/main/java/com/dev/holker/wholesale/Login.kt
@@ -1,12 +1,19 @@
 package com.dev.holker.wholesale
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        button.setOnClickListener {
+            val intent = Intent(applicationContext,List::class.java)
+            startActivity(intent)
+        }
     }
 }
