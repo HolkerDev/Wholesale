f375ca0992b1f4315365083f8ffbf7ebce092b51
HolkerDev <nick.penkovskiy@gmail.com>
2018-11-13 23:52:53 +0100

    Add possibility to order something

--- app/src/main/java/com/dev/holker/wholesale/Order.kt
+++ app/src/main/java/com/dev/holker/wholesale/Order.kt
@@ -2,11 +2,39 @@ package com.dev.holker.wholesale
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.parse.ParseObject
+import com.parse.ParseUser
+import kotlinx.android.synthetic.main.activity_order.*
 
 class Order : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
+
+        fun toast(string: String) {
+            Toast.makeText(applicationContext, string, Toast.LENGTH_LONG).show()
+        }
+
+        btn_submit.setOnClickListener { view ->
+            run {
+                val obj = ParseObject("Orders")
+                obj.put("name", et_productName.text.toString())
+                obj.put("amount", et_amount.text.toString())
+                obj.put("username", ParseUser.getCurrentUser().username)
+                obj.put("description", et_description.text.toString())
+                obj.saveInBackground {
+                    if (it == null) {
+                        toast("Fine!")
+                        finish()
+                    } else {
+                        toast(it.message.toString())
+                        finish()
+                    }
+                }
+            }
+        }
     }
 }
