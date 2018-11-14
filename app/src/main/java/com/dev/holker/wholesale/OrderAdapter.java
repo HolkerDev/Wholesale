143cc8779e88d776692d140418b2f1f8d755d69f
HolkerDev <nick.penkovskiy@gmail.com>
2018-11-14 12:21:53 +0100

    Add order description activity

--- app/src/main/java/com/dev/holker/wholesale/OrderAdapter.java
+++ app/src/main/java/com/dev/holker/wholesale/OrderAdapter.java
@@ -6,16 +6,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
 public class OrderAdapter extends ArrayAdapter<OrderItem> {
 
-    public Context mContext;
-    int mResource;
-    ArrayList<OrderItem> mObjects;
+    private Context mContext;
+    private int mResource;
+    private ArrayList<OrderItem> mObjects;
 
 
     public OrderAdapter(Context context, int resource, ArrayList<OrderItem> objects) {
@@ -30,19 +32,28 @@ public class OrderAdapter extends ArrayAdapter<OrderItem> {
     @NotNull
     @Override
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-//        return super.getView(position, convertView, parent);
 
         LayoutInflater inflater = LayoutInflater.from(mContext);
 
         View view = inflater.inflate(R.layout.item_order, null);
 
+        Button buttonCheck = view.findViewById(R.id.btn_check);
         TextView textViewProductName = view.findViewById(R.id.tv_product_name);
         TextView textViewProductAmount = view.findViewById(R.id.tv_amount_product);
 
-        OrderItem orderItem = mObjects.get(position);
+        final OrderItem orderItem = mObjects.get(position);
 
         textViewProductName.setText(orderItem.getProductName());
         textViewProductAmount.setText(orderItem.getProductAmount());
+
+        buttonCheck.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, orderItem.getProductName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
 
         return view;
 
