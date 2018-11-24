2e28537daa4bee838e0c1eac51e1d1199e26aa34
HolkerDev <nick.penkovskiy@gmail.com>
2018-11-24 22:04:21 +0100

    Add upload photo method

--- app/src/main/java/com/dev/holker/wholesale/activities/SignupDescription.kt
+++ app/src/main/java/com/dev/holker/wholesale/activities/SignupDescription.kt
@@ -1,7 +1,11 @@
 package com.dev.holker.wholesale.activities
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import com.dev.holker.wholesale.R
 import kotlinx.android.synthetic.main.activity_signup_description.*
 
@@ -12,5 +16,42 @@ class SignupDescription : AppCompatActivity() {
         setContentView(R.layout.activity_signup_description)
 
         val i = intent
+        val userType = intent.getStringExtra("typeOfUser")
+
+        //change hint of 'company name'
+        when (userType) {
+            "Client" -> {
+                et_signup_descr_name.hint = "Your name"
+            }
+            "Supplier" -> {
+                et_signup_descr_name.hint = "Name of your company"
+            }
+        }
+        btn_attach.setOnClickListener {
+            //            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+//                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+//            } else {
+//                getPhoto()
+//            }
+            getPhoto()
+        }
     }
+
+    fun getPhoto() {
+        val photoIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(photoIntent, 1);
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            val image = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, image)
+            profile_image.setImageBitmap(bitmap)
+        }
+
+    }
+
 }
