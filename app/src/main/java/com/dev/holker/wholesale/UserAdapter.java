19fda2c491abe78c3e594388929ffc89ca74ac52
HolkerDev <nick.penkovskiy@gmail.com>
2018-11-28 21:01:55 +0100

    Add User adapter

---
+++ app/src/main/java/com/dev/holker/wholesale/UserAdapter.java
@@ -0,0 +1,5 @@
+package com.dev.holker.wholesale;
+
+public class UserAdapter {
+    //TODO: Implement adapter
+}
--- app/src/main/java/com/dev/holker/wholesale/fragments/SearchFragment.kt
+++ app/src/main/java/com/dev/holker/wholesale/fragments/SearchFragment.kt
@@ -7,13 +7,38 @@ import android.view.View
 import android.view.ViewGroup
 import com.dev.holker.wholesale.R
 import com.dev.holker.wholesale.User
+import com.dev.holker.wholesale.UserAdapter
+import com.parse.ParseObject
+import com.parse.ParseQuery
+import com.parse.ParseUser
+import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : Fragment() {
 
-    lateinit var mUsers:ArrayList<User>
+    lateinit var mUsers: ArrayList<User>
+    lateinit var mAdapter:UserAdapter
 
     fun updateUserList() {
         mUsers.clear()
+        val query = ParseQuery<ParseObject>("User")
+        query.whereNotEqualTo("name", ParseUser.getCurrentUser().username)
+        query.findInBackground() { objects, e ->
+            run {
+                if (objects.size > 0 && e == null) {
+                    for (i: ParseObject in objects) {
+                        mUsers.add(User(i.getString("name")!!, i.getParseFile("avatar")!!, i.getString("description")!!))
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        lv_users.adapter
+        updateUserList()
+        //TODO: append adapter
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
