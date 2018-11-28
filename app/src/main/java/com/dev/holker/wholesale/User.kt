12c33c42961b5a35a415ee2fd49b1e6c1785b57f
HolkerDev <nick.penkovskiy@gmail.com>
2018-11-28 20:49:08 +0100

    Add User class

---
+++ app/src/main/java/com/dev/holker/wholesale/User.kt
@@ -0,0 +1,6 @@
+package com.dev.holker.wholesale
+
+import com.parse.ParseFile
+
+class User constructor(val username: String, val avatar: ParseFile, val description: String) {
+}
--- app/src/main/java/com/dev/holker/wholesale/fragments/SearchFragment.kt
+++ app/src/main/java/com/dev/holker/wholesale/fragments/SearchFragment.kt
@@ -6,8 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dev.holker.wholesale.R
+import com.dev.holker.wholesale.User
 
 class SearchFragment : Fragment() {
+
+    lateinit var mUsers:ArrayList<User>
+
+    fun updateUserList() {
+        mUsers.clear()
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_search, null)
     }
