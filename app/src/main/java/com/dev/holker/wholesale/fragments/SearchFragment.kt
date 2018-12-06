fc2870f6fc7e671c2dda8036809de7d86dd6296d
HolkerDev <nick.penkovskiy@gmail.com>
2018-12-06 23:16:47 +0100

    Add rxjava module to search function

--- app/src/main/java/com/dev/holker/wholesale/fragments/SearchFragment.kt
+++ app/src/main/java/com/dev/holker/wholesale/fragments/SearchFragment.kt
@@ -26,7 +26,7 @@ class SearchFragment : Fragment() {
         Toast.makeText(context, string, Toast.LENGTH_SHORT).show()
     }
 
-    fun updateUserList() {
+    fun updateUserList(filter: String) {
         mUsers.clear()
         val query = ParseUser.getQuery()
         query.whereNotEqualTo("username", ParseUser.getCurrentUser().username)
@@ -35,7 +35,11 @@ class SearchFragment : Fragment() {
                 if (e == null) {
                     if (objects.size > 0) {
                         for (i: ParseUser in objects) {
-                            mUsers.add(i.username)
+                            if (!filter.equals("")) {
+                                Log.i("MyLog", "Filter : $filter")
+                            } else {
+                                mUsers.add(i.username)
+                            }
                         }
                         lv_users.adapter = mAdapter
                     } else {
@@ -54,7 +58,7 @@ class SearchFragment : Fragment() {
         mUsers = arrayListOf<String>()
 
         mAdapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, mUsers);
-        updateUserList()
+        updateUserList("")
 
         io.reactivex.Observable.create(ObservableOnSubscribe<String> { subscriber ->
             searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
@@ -74,7 +78,7 @@ class SearchFragment : Fragment() {
             .distinct()
             .filter { text -> text.isNotBlank() }
             .subscribe { text ->
-                Log.d("MyLog", "subscriber: $text")
+                updateUserList(text)
             }
 
     }
