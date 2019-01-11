cec6035095332e5e13195c5c4c2de15c6de8b906
HolkerDev <nick.penkovskiy@gmail.com>
2019-01-11 19:58:27 +0100

    Fix order addition

--- app/src/main/java/com/dev/holker/wholesale/activities/Order.kt
+++ app/src/main/java/com/dev/holker/wholesale/activities/Order.kt
@@ -1,20 +1,76 @@
 package com.dev.holker.wholesale.activities
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
 import com.dev.holker.wholesale.R
 import com.dev.holker.wholesale.presenters.OrderPresenter
+import com.parse.ParseUser
 import kotlinx.android.synthetic.main.activity_order.*
 
+
 class Order : AppCompatActivity() {
+    var productType = "None"
+    lateinit var photo: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
 
+        photo = BitmapFactory.decodeResource(resources, R.drawable.unknown)
+
+        btn_select_type.setOnClickListener {
+            val dialogOrder: AlertDialog
+            val items = arrayOf(
+                "Dining Sets", "Outdoor Lounge Chairs", "Banquet Chairs", "Tables",
+                "High chairs", "Bar Stools"
+            )
+            val builder = AlertDialog.Builder(this@Order)
+            builder.setTitle("Select background")
+            builder.setSingleChoiceItems(items, -1) { _, which ->
+                val choice = items[which]
+                productType = items[which]
+            }
+            builder.setPositiveButton("Submit") { _, _ ->
+            }
+            dialogOrder = builder.create()
+            dialogOrder.show()
+        }
+
+        //Attach photo to order
+        btn_send_photo.setOnClickListener {
+            getPhoto()
+        }
+
         val presenter = OrderPresenter(findViewById(android.R.id.content))
+        //create order
         btn_submit.setOnClickListener {
-            presenter.submit(et_productName.toString(), et_amount.text.toString().toInt(), et_description.toString())
+            presenter.submit(
+                et_productName.text.toString(),
+                et_amount.text.toString().toInt(),
+                et_description.text.toString(),
+                ParseUser.getCurrentUser(),
+                productType,
+                photo
+            )
+        }
+    }
+
+    fun getPhoto() {
+        val photoIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(photoIntent, 2);
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null) {
+            val image = data.data
+            photo = MediaStore.Images.Media.getBitmap(contentResolver, image)
         }
     }
 }
--- app/src/main/java/com/dev/holker/wholesale/presenters/OrderPresenter.kt
+++ app/src/main/java/com/dev/holker/wholesale/presenters/OrderPresenter.kt
@@ -1,29 +1,63 @@
 package com.dev.holker.wholesale.presenters
 
 import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.graphics.Bitmap
 import android.view.View
 import android.widget.Toast
-import com.dev.holker.wholesale.presenters.interfaces.IOrderPresenter
+import com.parse.ParseFile
 import com.parse.ParseObject
+import com.parse.ParseQuery
+import com.parse.ParseUser
+import java.io.ByteArrayOutputStream
 
-class OrderPresenter(val view: View) : IOrderPresenter {
-    override fun toast(string: String) {
+
+class OrderPresenter(val view: View) {
+    fun toast(string: String) {
         Toast.makeText(view.context, string, Toast.LENGTH_SHORT).show()
     }
 
-    override fun submit(name: String, amount: Int, descr: String) {
+    fun submit(name: String, amount: Int, descr: String, user: ParseUser, productType: String, photo: Bitmap) {
         val obj = ParseObject("Order")
         obj.put("name", name)
         obj.put("amount", amount)
         obj.put("description", descr)
+
+        val queryType = ParseQuery<ParseObject>("ProductType")
+        queryType.whereEqualTo("name", productType)
+        val type = queryType.first
+
+        val stream = ByteArrayOutputStream()
+        photo.compress(Bitmap.CompressFormat.PNG, 100, stream)
+        val bytes = stream.toByteArray()
+        val photoFile = ParseFile(bytes, "photo.png")
+        obj.put("photo", photoFile)
+        obj.put("user", user)
+        obj.put("productType", type)
+        obj.put("status", "In process")
+
         obj.saveInBackground {
             if (it != null) {
                 toast(it.message.toString())
             } else {
                 toast("Order is ready!")
-                val activity = view.context as Activity
-                activity.finish()
+                val activity = scanForActivity(view.context)
+                if (activity != null) {
+                    activity.finish()
+                }
             }
         }
+    }
+
+    private fun scanForActivity(cont: Context?): Activity? {
+        if (cont == null)
+            return null
+        else if (cont is Activity)
+            return cont
+        else if (cont is ContextWrapper)
+            return scanForActivity((cont as ContextWrapper).baseContext)
+
+        return null
     }
 }
