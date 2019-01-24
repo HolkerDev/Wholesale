ce8fb73b72ea0c3473dafdc7e7c2967d45bc4277
HolkerDev <nick.penkovskiy@gmail.com>
2019-01-24 21:35:43 +0100

    add chat activity and simple design

---
+++ app/src/main/java/com/dev/holker/wholesale/activities/ChatActivity.kt
@@ -0,0 +1,13 @@
+package com.dev.holker.wholesale.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dev.holker.wholesale.R
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
